810814439f1d00abc1bba905740b4cd89277ec2f
marcusgik <qwertyditt@gmail.com>
2026-05-15 10:28:43 +0300

    menu-update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const compression = require('compression');
 const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
+app.use(compression());
 const port = 3000;
 
 app.use(express.static(__dirname, {
